c25488956f70b40b8d513c10b22f0cda995f00d1
andwiput <andyginting94@gmail.com>
2021-07-11 23:35:02 +0700

    initialize cra and add axios

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_SPOTIFY);
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
